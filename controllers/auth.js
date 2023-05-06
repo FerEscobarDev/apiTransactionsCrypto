617dee0a02cfbf974393c886ebaa67f9053c87f7
FerEscobarDev <jeiesc04@outlook.com>
2023-05-05 20:36:26 -0500

    Avances Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,73 @@
 const { response } = require('express');
+const { PrismaClient } = require('@prisma/client');
+const { bcrypt } = require('bcryptjs')
+
+const prisma = new PrismaClient();
 
 const userCreate = async (req, res = response) => {
 
-    console.log(req.body);
-    /* await prisma.user.create({
-        data: {
-            name: 'Alice2',
-            email: 'alice2@prisma.io',
-            posts: {
-                create: { title: 'Hello World' },
-            },
-            profile: {
-                create: { bio: 'I like turtles' },
-            },
-        },
-    })
+    const { email, password } = req.body;
 
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-            profile: true,
-        },
-    })
+    try {
+        let user = await prisma.user.findUnique({
+                        where: {
+                            email: email,
+                        },
+                    })
+
+        if ( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe'
+            });
+        }
+
+        user = req.body;
+
+        
+    
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
+
+        console.log(user)
+        // await prisma.user.create({
+        //     data: user,
+        // })
+        // await usuario.save();
+
+        // // Generar JWT
+        // const token = await generarJWT( usuario.id, usuario.name );
+    
+        // res.status(201).json({
+        //     ok: true,
+        //     uid: usuario.id,
+        //     name: usuario.name,
+        //     token
+        // })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Ha ocurrido un error!, contacta al administrador'
+        });
+    }
+
+    // const user = req.body
+    // console.log(req.body);
+    
+
+    // const allUsers = await prisma.user.findMany({
+    //     include: {
+    //         posts: true,
+    //         profile: true,
+    //     },
+    // })
 
     await prisma.$disconnect()
-    console.dir(allUsers, { depth: null }) 
-    res.json(allUsers);  */
+    // console.dir(allUsers, { depth: null }) 
+    // res.json(allUsers);
     res.json({
         ok: true,
         msg: 'Register'
